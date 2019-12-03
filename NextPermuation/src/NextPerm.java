e1dddcefc21e0ade42086f41cba4293d5ae8cf31
RileyWium <rjwium@gmail.com>
2019-12-02 20:43:41 -0800

    added time completion

--- NextPermuation/src/NextPerm.java
+++ NextPermuation/src/NextPerm.java
@@ -2,7 +2,7 @@
  Given an integer, find the next permutation of it in absolute order.
   For example, given 48975, the next permutation would be 49578.
  
- 
+ 1:52 hr
  */
 import java.util.*;
 public class NextPerm {
