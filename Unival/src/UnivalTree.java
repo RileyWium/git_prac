dceb3539645fb826493081cb07b9f5563fb92a3b
RileyWium <rjwium@gmail.com>
2020-05-08 15:56:57 -0700

    Finished Unival Tree

--- Unival/src/UnivalTree.java
+++ Unival/src/UnivalTree.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 /*
-A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+A unival tree (which stands for "universal value") is a tree where all nodes have the same value.
 
 Given the root to a binary tree, count the number of unival subtrees.
 
@@ -16,7 +16,7 @@ For example, the following tree has 5 unival subtrees:
  1   1
 
 
-time:
+time: 1:54hr
 easy
  */
 
@@ -25,20 +25,20 @@ public class UnivalTree {
 	public static void main(String[] args) {
 		
 		UnivalTree inst = new UnivalTree();
-		TreeNode tn = inst.createTree();
+		BinaryTreeNode tn = inst.createTree();
 		
-		int output = inst.countUnivalSubTrees(0, tn);
+		int output = inst.countSubTrees(tn).subTreeNum;
 		System.out.println("The following Tree has "+output+" unival subtrees");
 	}
 	
-	public TreeNode createTree() {
-		TreeNode tn = new TreeNode(0);
-		TreeNode t1 = new TreeNode(1);
-		TreeNode t2 = new TreeNode(0);
-		TreeNode t3 = new TreeNode(1);
-		TreeNode t4 = new TreeNode(0);
-		TreeNode t5 = new TreeNode(1);
-		TreeNode t6 = new TreeNode(1);
+	public BinaryTreeNode createTree() {
+		BinaryTreeNode tn = new BinaryTreeNode(0);
+		BinaryTreeNode t1 = new BinaryTreeNode(1);
+		BinaryTreeNode t2 = new BinaryTreeNode(0);
+		BinaryTreeNode t3 = new BinaryTreeNode(1);
+		BinaryTreeNode t4 = new BinaryTreeNode(0);
+		BinaryTreeNode t5 = new BinaryTreeNode(1);
+		BinaryTreeNode t6 = new BinaryTreeNode(1);
 		
 		tn.addChildren(t1,t2);
 		t2.addChildren(t3, t4);
@@ -46,28 +46,68 @@ public class UnivalTree {
 		return tn;
 	}
 	
-	public int countUnivalSubTrees(int val, TreeNode tn) {
+	public UnivalReturnType countSubTrees(BinaryTreeNode tn) {
+		UnivalReturnType output = new UnivalReturnType(0, false);
 		
+		if(tn.btLeft == null && tn.btRight == null) {
+			output.subTreeNum++;
+			output.isUnival = true;
+		}else {
+			boolean childrenUnival = true;
+			if(tn.btLeft != null) {
+				UnivalReturnType left =countSubTrees(tn.btLeft);
+				output.subTreeNum+=left.subTreeNum;
+				if(left.isUnival) {					
+					if(tn.val != tn.btLeft.val) {
+						childrenUnival = false;
+					}
+				}else {
+					childrenUnival =false;
+				}
+			}
+			if(tn.btRight != null) {
+				UnivalReturnType right =countSubTrees(tn.btRight);
+				output.subTreeNum+=right.subTreeNum;
+				if(right.isUnival) {
+					if(tn.val != tn.btRight.val) {
+						childrenUnival = false;
+					}
+				}else {
+					childrenUnival =false;
+				}
+			}
+			if(childrenUnival) {
+				output.subTreeNum++;
+				output.isUnival = true;
+			}
+		}		
 		
-		return 0;
+		return output;
 	}
+	
+	private class UnivalReturnType{
+		public int subTreeNum;
+		public boolean isUnival;
 		
-	private class TreeNode{
+		public UnivalReturnType(int x, boolean y) {
+			subTreeNum = x;
+			isUnival = y;
+		}
+	}
+	
+	private class BinaryTreeNode{
 		
 		public int val = 0;
-		public ArrayList<TreeNode> children;
+		public BinaryTreeNode btLeft;
+		public BinaryTreeNode btRight;
 		
-		public TreeNode(int x) {
+		public BinaryTreeNode(int x) {
 			val = x;
 		}
 		
-		public void addChild(TreeNode tn) {
-			children.add(tn);
-		}
-		
-		public void addChildren(TreeNode t1, TreeNode t2) {
-			children.add(t1);
-			children.add(t2);
+		public void addChildren(BinaryTreeNode t1, BinaryTreeNode t2) {
+			btLeft = (t1);
+			btRight = (t2);
 		}		
 	}
 }
